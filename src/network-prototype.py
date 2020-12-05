abdce4071d1ae84d106b37cbf6317fb84d444859
TimothyBruce <tim.bruce9@gmail.com>
2020-12-04 23:23:43 -0500

    Added neural network and training tool

---
+++ src/network-prototype.py
@@ -0,0 +1,84 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+from keras import layers
+import numpy as np
+import pickle
+
+maxlen = 100
+
+policy_data_file = pickle.load(open("policy_data.p", "rb"))
+sentences = []
+labels = []
+for policy in policy_data_file:
+    for paragraph in policy['policy_text']:
+        if policy['trained_key'][policy['policy_text'].index(paragraph)] != None:
+            sentences.append(paragraph)
+            labels.append(1 if policy['trained_key'][policy['policy_text'].index(paragraph)] == 'y' else 0)
+sentences = np.array(sentences)
+labels = np.array(labels)
+sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, labels, test_size=0.25, random_state=1000)
+
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath) as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word]
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+
+    return embedding_matrix
+
+embedding_dim = 50
+
+embedding_matrix = create_embedding_matrix('glove.6B.50d.txt',tokenizer.word_index, embedding_dim)
+
+nonzero_elements = np.count_nonzero(np.count_nonzero(embedding_matrix, axis=1))
+print("Fraction of corpus covered by preloaded vocab %s" % (nonzero_elements / vocab_size, ))
+
+model = Sequential()
+model.add(layers.Embedding(vocab_size, embedding_dim, input_length=maxlen))
+model.add(layers.Conv1D(128, 5, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=10,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+val = np.ndarray.tolist(model.predict(X_test))
+for prediction in val:
+    if prediction[0] > 0.5:
+        print(sentences_test[val.index(prediction)])
+        print(prediction)
+        print()
---
+++ src/network_data_collection.py
@@ -0,0 +1,128 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import selenium.common.exceptions as se
+import re
+import pickle
+
+
+# This function draws from the links.txt file for URLs.
+def collect_and_save_data():
+    driver = webdriver.Chrome()
+
+    policies = []
+    url_file = open('links.txt', "r")           # Read link file
+    for url in url_file.readlines():            # For every url, instantiate the Policy class with the url
+        policies.append(Policy(url, driver))
+
+    # policies.append(Policy(url_file.readlines()[1], driver))
+
+    url_file.close()                            # Close the browser and file
+    driver.close()
+
+    policy_data = []
+    for policyObject in policies:               # Output verification
+        if policyObject.extracted_policy is not None:
+            policy_text = []
+            for string in policyObject.extracted_policy.body.strings:
+                string = string.rstrip().replace("\n", "").replace("\t", "")
+                policy_text.append(string)
+            print("Extracted " + policyObject.policy_url)
+            policy_data.append({"policy_homepage": policyObject.url,
+                                "policy_url": policyObject.policy_url,
+                                "trained_key": [None for i in range(len(policy_text))],
+                                "policy_text": policy_text})
+
+    save_array_to_file(policy_data, "policy_data.p")
+
+    # Possible string collection commands
+    # policyObject.extracted_policy.find_all('p')
+
+opt_identifiers = ['you may', 'setting', 'you can', 'deactivate', 'revoke', 'chang', 'opt out', 'opt in',
+                   'opt-out', 'opt-in', 'how to', 'update', 'manage', 'choice', 'opting', 'consent', 'disable',
+                   'withdraw', 'permission', 'sign a', 'signed', 'rights', 'right', 'cookie', 'unsubscribe']
+
+def tag_trainning_data():
+    data = get_array_from_file("policy_data.p")
+    for policy in data:
+        for paragraph in policy['policy_text']:
+            if policy['trained_key'][policy['policy_text'].index(paragraph)] == None:
+                flag = False
+                for identifier in opt_identifiers:
+                    if identifier in paragraph:
+                        flag = identifier
+                if flag is False:
+                    policy['trained_key'][policy['policy_text'].index(paragraph)] = 'n'
+                else:
+                    print(paragraph)
+                    print("Identified based on %s" % (flag, ))
+                    x = input("Is this an opt statement? (y/n) (q to quit)\n")
+                    if x == "y" or x == "n":
+                        policy['trained_key'][policy['policy_text'].index(paragraph)] = x
+                        print("Response noted")
+                        print("Currently %s%s done with this policy.\n\n" %
+                              ((policy['policy_text'].index(paragraph) + 1) / len(policy['policy_text']), "%"))
+                    elif x == "q":
+                        save_array_to_file(data, "policy_data.p")
+                        return 1
+        print("Policy %s completed." % (policy['policy_url'], ))
+    print("Completed all data. Terminating Program")
+    return 1
+
+
+# Class for storing policy information
+class Policy:
+    def __init__(self, website_url, selenium_driver, policy_url=None, extracted_policy=None):
+        self.driver = selenium_driver           # Selenium driver for extracting information.
+        self.url = website_url                  # Url of the website's main page
+        if policy_url is not None:
+            self.policy_url = policy_url        # URL of the privacy policy
+        else:
+            self.policy_url = self.retrieve_policy_url()
+
+        if extracted_policy is not None:
+            self.extracted_policy = extracted_policy    # BeautifulSoup of the privacy policy
+        else:
+            self.extracted_policy = self.retrieve_policy()
+
+    # Extract the privacy policy URL from the homepage of the website.
+    def retrieve_policy_url(self):
+        self.driver.get(self.url)
+        policy_html = self.driver.page_source
+        soup = BeautifulSoup(policy_html, 'html.parser')
+
+        links = soup.find_all('a')
+
+        for item in links:
+            if re.search(r"privacy", str(item.get_text()), re.IGNORECASE):
+                if item.get('href')[0:4] != "http":
+                    if item.get('href')[0:2] == "//":
+                        return "http:" + item.get('href')
+                    else:
+                        return self.url.rstrip() + item.get('href')
+                return item.get('href')
+        return None
+
+    # Extract the privacy policy from the URL.
+    def retrieve_policy(self):
+        if self.policy_url is not None:
+            try:
+                self.driver.get(self.policy_url)
+            except se.InvalidArgumentException:
+                print("Failed " + self.policy_url)
+                return None
+        else:
+            return None
+        policy_html = self.driver.page_source
+        soup = BeautifulSoup(policy_html, 'html.parser')
+        return soup
+
+
+def save_array_to_file(array, filename):
+    pickle.dump(array, open(filename, "wb"))
+
+
+def get_array_from_file(filename):
+    return pickle.load(open(filename, "rb"))
+
+#collect_and_save_data()
+tag_trainning_data()
